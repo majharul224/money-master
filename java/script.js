da19f3aea949e6b80a39ba4e55878641b09e38bc
majharul224 <majharulislam2225@gmail.com>
2022-02-17 23:48:44 +0600

    first commit

--- java/script.js
+++ java/script.js
@@ -1,40 +1,49 @@
+
+
+
+
 const btnCalculate = document.getElementById('calculate');
-const incomeInput = document.getElementById('income-input');
-const foodInput = document.getElementById('food-input');
-const rentInput = document.getElementById('rent-input');
-const clothesInput = document.getElementById('clothes-input');
+const income = document.getElementById('income-input');
+const food = document.getElementById('food-input');
+const rent = document.getElementById('rent-input');
+const clothes = document.getElementById('clothes-input');
 const totoalExprese = document.getElementById('totoal-exprese');
 const balance = document.getElementById('balance');
-const latestMoney = document.getElementById ('latest-money');
-const Percentage = document.getElementById('Percentage');
-const saveButton = document.getElementById('save-money');
+const remBalance = document.getElementById ('rem-balance');
+const persent = document.getElementById('Percentage');
+const save = document.getElementById('save-money');
 const saveAmount = document.getElementById('save-amount');
 const Remaining = document.getElementById('rem-balance');
 const allInput = parseInt(document.querySelectorAll('input'));
 
 
+
 btnCalculate.addEventListener('click',calculateHandler); 
+
+
 
 
 function calculateHandler() {
 
-   // console.log("hello");
-    const foodBalance = parseInt(foodInput.value);
-    const rentBalance = parseInt(rentInput.value); value
-    const clothseBalance = parseInt(clothesInput.value); 
-    const myIncome = parseInt(income.value); 
-
-    const totoalExpreseBalance = foodBalance + rentBalance + clothseBalance ; 
-   
-  totoalExprese.innerText = totoalExpreseBalance; 
-     const newBalance = myIncome  - totoalExpreseBalance ; 
-    balance.innerText = totoalExpreseBalance; 
     
-   return newBalance; 
+   const spendForFood = parseInt(food.value);
+   const spendForRent = parseInt(rent.value); 
+   const spendForClothse = parseInt(clothes.value);
+   const myIncome = parseInt(income.value); 
+
+   
+
+    const totoalSpend = spendForFood + spendForClothse + spendForRent ; 
+
+    totoalExprese.innerText = totoalSpend; 
+
+    const newBalance = myIncome  - totoalSpend ; 
+    balance.innerText = newBalance; 
+    
+    return newBalance; 
    
 
 }
-
 
 save.addEventListener('click', saveMoney);     
 
@@ -45,19 +54,20 @@ function saveMoney(){
     const numpersent = parseInt(persent.value);
    
     
-   let persentage = myIncome / 100 ; 
+    let persentage = myIncome / 100 ; 
 
     const finulSaveAmmount =  persentage * numpersent ;
 
     
 
-   if (finulSaveAmmount <= calculateHandler()) {
+    if (finulSaveAmmount <= calculateHandler()) {
         saveAmount.innerText = finulSaveAmmount;
 
         Remaining.innerText = calculateHandler() - finulSaveAmmount;
-   }
-   else{
-       alert('apnar account e porjapto poriman balance nai')
+    }
+    else{
+        alert('apnar account e porjapto poriman balance nai')
     }
 
+    
 }
